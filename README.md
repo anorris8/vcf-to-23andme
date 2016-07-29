4fbb29a37567dcf3051007c9381ea2996906a42a
Seán <git@2sh.me>
2016-07-29 11:00:56 +0100

    Add func to allow compressed file input

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # VCF-to-23andMe
 These scripts convert an imputed DNA.Land VCF into the 23andMe V3 raw data format.
 
-Firstly, data_to_db.py converts the VCF file and any additional 23andMe raw data file (adds 23andMe indentifiers) into an indexed SQLite3 database for quick searching. db_to_23.py then inserts genotypes into the blank V3 file retrieved from the database by chromosome, position and identifier.
+data_to_db.py is used to convert the VCF file and any additional 23andMe raw data file (adds 23andMe indentifiers) into an indexed SQLite3 database for quick searching. db_to_23.py then inserts genotypes into the blank V3 file retrieved from the database by chromosome, position and identifier.
+
+The data_to_db.py script accepts both compressed and uncompressed data files.
 
 ##Requirements
 * Python3
@@ -15,7 +17,7 @@ python db_to_23.py <INPUT_DB> <INPUT_BLANK_23_RAW> <OUTPUT_23_RAW>
 
 ##Usage Example
 ```
-python data_to_db.py imputed.vcf vcf genome.db
-python data_to_db.py 23andme_v4_genome.txt 23andme genome.db
+python data_to_db.py imputed.vcf.gz vcf genome.db
+python data_to_db.py genome_Fred_Bloggs_v4_Full_20160423012345.zip 23andme genome.db
 python db_to_23.py genome.db blank_v3.txt genome_Fred_Bloggs_Full_20140628012345.txt
 ```
--- data_to_db.py
+++ data_to_db.py
@@ -2,9 +2,10 @@
 # Copyright (c) 2015-2016 Sean Hewitt <contact@SeanHewitt.com>
 # Licensed under the MIT license: http://SeanHewitt.com/MIT-LICENSE.txt
 
-import sys
-import csv
-import sqlite3
+import sys, io
+import gzip, zipfile
+import csv, sqlite3
+
 from sys import argv
 
 csv.field_size_limit(sys.maxsize)
@@ -34,53 +35,88 @@ on
 
 insert_statement = "insert or replace into genome values (?,?,?,?)"
 
-with open(data_file_name, "r", encoding="utf8") as data_file:
-	reader = csv.reader(data_file, delimiter="\t", quoting=csv.QUOTE_NONE)
-	for i, line in enumerate(reader):
-		if not line or line[0].startswith("#"):
+def open_file(file_name):
+	data_file = gzip.open(file_name, "rt", encoding="utf8")
+	try:
+		data_file.read(1);
+	except OSError:
+		data_file.close()
+	else:
+		data_file.seek(0);
+		return data_file
+		
+	try:
+		archive = zipfile.ZipFile(file_name)
+	except zipfile.BadZipFile:
+		pass
+	else:
+		for object_name in archive.namelist():
+			if not object_name.endswith("/"):
+				break
+		else:
+			raise Exception("ZIP Archive has no readable file")
+		
+		data_file = io.TextIOWrapper(archive.open(object_name), encoding="utf8")
+		archive.close()
+		return data_file
+		
+	return open(file_name, "r", encoding="utf8")
+
+try:
+	data_file = open_file(data_file_name)
+except Exception as e:
+	print(e)
+	exit()
+
+reader = csv.reader(data_file, delimiter="\t", quoting=csv.QUOTE_NONE)
+print("Processing " + data_format + " data file...")
+for i, line in enumerate(reader):
+	if not line or line[0].startswith("#"):
+		continue
+	
+	if data_format == "vcf":
+		if len(line) < 10 or line[0][0] == "<":
+			continue
+		try:
+			gt_data_index = line[8].split(":").index("GT")
+		except:
 			continue
 		
-		if data_format == "vcf":
-			if len(line) < 10 or line[0][0] == "<":
-				continue
-			try:
-				gt_data_index = line[8].split(":").index("GT")
-			except:
-				continue
-			
-			bases = [line[3]] + line[4].split(",")
-			
-			genotype = ""
-			base_indexes = line[9].split(":")[gt_data_index].replace("|", "/").split("/")
-			valid = False
-			for base_index in base_indexes:
-				if base_index == ".":
-					break
-				base = bases[int(base_index)]
-				if len(base) > 1:
-					break
-				genotype += base[0]
-			else:
-				valid = True
-			
-			if not valid:
-				continue
-				
-			row = line[:3]
-			row.append(genotype)
-			
-		elif data_format == "23andme":
-			if len(line) < 4:
-				continue
-			row = (line[1], line[2], line[0], line[3])
-			
-		else:
-			print("Unknown data format: " + data_format)
-			exit()
+		bases = [line[3]] + line[4].split(",")
 		
-		cursor.execute(insert_statement, row)
+		genotype = ""
+		base_indexes = line[9].split(":")[gt_data_index].replace("|", "/").split("/")
+		valid = False
+		for base_index in base_indexes:
+			if base_index == ".":
+				break
+			base = bases[int(base_index)]
+			if len(base) > 1:
+				break
+			genotype += base[0]
+		else:
+			valid = True
+		
+		if not valid:
+			continue
+			
+		row = line[:3]
+		row.append(genotype)
+		
+	elif data_format == "23andme":
+		if len(line) < 4:
+			continue
+		row = (line[1], line[2], line[0], line[3])
+		
+	else:
+		print("Unknown data format: " + data_format)
+		exit()
+	
+	cursor.execute(insert_statement, row)
 
-print("Lines:", i)
+print("Lines read:", i+1)
+
+data_file.close()
 
 conn.commit()
 conn.close()
